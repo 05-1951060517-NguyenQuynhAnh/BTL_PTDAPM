dc6436e9c8d92c311319e9373bdc8283b3473378
05-1951060517-NguyenQuynhAnh <qanh1662001@gmail.com>
2023-02-24 02:09:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="public/style.css">
+    <title>GUCCI Official</title>
+    <link rel="shortcut icon" href="img/web.png">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container-fluid px-5" style="background:#f6f1eb;">
+            <div class="px-5  py-2">
+                <div class="row">
+                    <div class="col py-3" style="font-size:13px;">
+                        NHOM7@GMAIL.COM | Hotline: 0837 473 645
+                    </div>
+
+                    <div class="col d-flex justify-content-center">
+                        <div class=""> <img src="img/logo.png" style="weight:100px;height:66px" alt=""></div>
+
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <div class="mt-3">
+                            <a href="login.php" class="mx-auto text-decoration-none link-dark ">
+                                <div class="d-flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
+                                        class="me-2 bi bi-person" viewBox="0 0 16 16">
+                                        <path
+                                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                                    </svg>
+                                    <p class="pt-1" style="font-size:14px">Đăng nhập</p>
+                                </div>
+
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="p-5">
+                    <div class="" style="padding-left:200px;padding-right:200px;">
+
+                        <div class="text-center mt-3">
+                            <font STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="6">VŨ TRỤ
+                                THỜI TRANG</font>
+                            <p class="mt-3" style="font-family:courier,arial,helvetica;">
+                                Cách bạn ăn mặc thật sự phản ánh cảm nhận, lối sống, kiến
+                                thức và cả lựa chọn của bạn. Đó là điều tôi muốn đưa vào các thiết kế của Gucci.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="px-5">
+                <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="3000">
+                            <img src="img/panel.avif" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>GUCCI VALIGERIA</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <img src="img/panel1.avif" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>COLLECTION PRE-LAUNCH</h5>
+                                <h1>WOMEN'S FALL WINTER 2023 FASHION SHOW</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <img src="img/panel2.avif" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>GUCCI SPRING SUMMER 2023</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <img src="img/panel3.avif" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>GG SUPREME FLORA</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="mt-5 pt-5">
+                    <div class="text-center mt-3">
+                        <font STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="6">SẢN PHẨM KÌ DIỆU
+                            CỦA CHÚNG TÔI</font>
+                    </div>
+                </div>
+                <div class="row mx-5 mt-4">
+                    <div class="col px-0 me-4">
+                        <a href="" class="text-decoration-none">
+                            <img class="img-fluid rounded-3 mb-2" src="img/loai.png" alt="">
+                            <font class="link-dark"STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="3">PHỤ KIỆN</font>
+                        </a>
+                    </div>
+
+                    <div class="col px-0 me-4">
+                        <a href="product/bottom.php" class="text-decoration-none">
+                            <img class="img-fluid rounded-3 mb-2" src="img/loai1.png" alt="">
+                            <font class="link-dark"STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="3">GIÀY</font>
+                        </a>
+                       
+                    </div>
+
+                    <div class="col  px-0 me-4">
+                        <a href="product/skirt.php" class="text-decoration-none">
+                            <img class="img-fluid rounded-3 mb-2" src="img/loai2.png" alt="">
+                            <font class="link-dark"STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="3">TÚI XÁCH</font>
+                        </a>
+                       
+                    </div>
+
+                    <div class="col px-0">
+                        <a href="product/phukien.php" class="text-decoration-none">
+                            <img class="img-fluid rounded-3 mb-2" src="img/loai3.png" alt="">
+                            <font class="link-dark"STYLE="letter-spacing: 2.75px;word-spacing:2px" face="Candara" size="3">QUẦN ÁO</font>
+                        </a>
+                       
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
